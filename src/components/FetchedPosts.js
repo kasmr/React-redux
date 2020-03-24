ff8add16b4c48be361a42f7abaea02adac103f90
kasmr <alikkras@gmail.com>
2020-03-24 23:31:50 +0300

    added loading

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Post from './Post';
+import Spinner from './Spinner';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchPosts, setLoading } from '../redux/actions';
+import { fetchPosts } from '../redux/actions';
 
 const FetchedPosts = () => {
   const dispatch = useDispatch();
   const posts = useSelector(state => state.posts.fetchedPosts);
   const loading = useSelector(state => state.app.loading);
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   if (!posts.length) {
     return (
       <button
         className='btn btn-primary'
-        onClick={() => dispatch(fetchPosts(), setLoading())}
+        onClick={() => dispatch(fetchPosts())}
       >
         Загрузить
       </button>
     );
-  }
-  if (loading) {
-    return <h1>Загрузка...</h1>;
   } else {
     return posts.map(post => <Post post={post} key={post.id} />);
   }
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, FETCH_POST, SHOW_LOADER } from './types';
+import { CREATE_POST, FETCH_POST, SHOW_LOADER, HIDE_LOADER } from './types';
 
 export function createPost(post) {
   return {
@@ -7,19 +7,26 @@ export function createPost(post) {
   };
 }
 
-export function setLoading(loading) {
+export function showLoading() {
   return {
-    type: SHOW_LOADER,
-    payload: true
+    type: SHOW_LOADER
+  };
+}
+
+export function hideLoading() {
+  return {
+    type: HIDE_LOADER
   };
 }
 
 export function fetchPosts() {
   return async dispatch => {
+    dispatch(showLoading());
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/posts?_limit=5'
     );
     const json = await response.json();
     dispatch({ type: FETCH_POST, payload: json });
+    dispatch(hideLoading());
   };
 }
